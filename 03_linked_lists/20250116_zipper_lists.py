d9747a2d499272e4a4ec848475c94784f12ce70e
echo29er <sbassettuk@gmail.com>
2025-01-19 21:01:17 +0000

    Zipper list

--- 03_linked_lists/20250116_zipper_lists.py
+++ 03_linked_lists/20250116_zipper_lists.py
@@ -25,7 +25,6 @@ def zipper_lists (head_1: Node, head_2: Node) -> Node:
 
         Do this in-place, by mutating the original Nodes.
 
-        You may assume that both input lists are non-empty.
 
     Args:
         head_1 (Node): the head of a linked list
@@ -35,45 +34,49 @@ def zipper_lists (head_1: Node, head_2: Node) -> Node:
         Node i.e. the head of the merged linked list
 
     Assumptions: 
-        N/A
+        Both input lists are non-empty
 
     Time complexity: 
-        O(
+        O(min(m,n)) our traversal length is equal to the length of the smallest list. When one list is exhausted we simply point to the rest of the remaining contiguous list in O(1)
 
     Space complexity: 
-        O(
+        O(1) as we are mutating this list in place.
     """
 
 
+    # Using three pointers: one to track the last node of the list (tail), one to track the position on the first_list, and one to track the position of the second_list. 
+    # tail - we keep track of the last node of the list in order to append the remainder of the list when one list becomes empty. 
+    # first_list_pointer - we track the current node of the element in the first linked list. 
+    # second_list_pointer - we track the current node of the element in the second linked list.
 
-    # Keep track of the end of the tail, the next node and the 
+    # 
     tail = head_1
-    current_1 = head_1.next # start at the second node of list1
-    current_2 = head_2 # start at the first node of list2
+    first_list_pointer = head_1
+    second_list_pointer = head_2
 
-    while current_1 is not None and current_2 is not None:
+    while first_list_pointer is not None and second_list_pointer is not None:
         
         # example with head_1 = A -> B -> C -> D and head_2 = 1 -> 2 -> 3 -> 4
 
         # Store original node of each node's next node
-        original_head_1_next = head_1.next # B | C | D
-        original_head_2_next = head_2.next # 2 | 3 | 4
+        first_list_pointer_next = first_list_pointer.next # B | C | D
+        second_list_pointer_next = second_list_pointer.next # 2 | 3 | 4
 
-        # Zip the nodes i.e. join them 
-        head_1.next = head_2 # A -> 1 | B -> 2 | C -> 3
-        head_2.next = original_head_1_next # 1 -> B | 2 -> C | C -> 4
+        # Repoint the current nodes to the correct next node
+        first_list_pointer.next = second_list_pointer # A -> 1 | B -> 2 | C -> 3
+        second_list_pointer.next = first_list_pointer_next # 1 -> B | 2 -> C | C -> 4
 
         # Move the tail to the last node we attached
-        tail = head_2
+        tail = second_list_pointer
 
         # Move the nodes
-        head_1 = head_2.next # Move to B | Move to C
-        head_2 = original_head_2_next # Move to 2 | Move to 3
+        first_list_pointer = second_list_pointer.next # Move to B | Move to C
+        second_list_pointer = second_list_pointer_next # Move to 2 | Move to 3
 
-    if head_1 is not None: # i.e. head_1 still has nodes
-        tail.next = head_1 # point head_2 to the current head_1 
-    if head_2 is not None: # i.e. head_2 sitll has nodes
-        tail.next = head_2 # point head_1 to the current head_2 
+    if first_list_pointer is not None: # List 2 was exhausted, attach remainder of list 1
+        tail.next = first_list_pointer 
+    if second_list_pointer is not None: # List 1 was exhausted, attach remainder of list 2
+        tail.next = second_list_pointer 
 
 
     return head_1
@@ -120,8 +123,6 @@ def test_b():
     return zipper_lists(a, x)
     # a -> x -> b -> y -> c -> z -> d -> e -> f
 
-
-
 def test_c():
     s = Node("s")
     t = Node("t")
@@ -140,7 +141,36 @@ def test_c():
     return zipper_lists(s, one)
     # s -> 1 -> t -> 2 -> 3 -> 4
 
+def test_d():
+    w = Node("w")
+    # w
+
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    one.next = two
+    two.next = three
+    # 1 -> 2 -> 3 
+
+    return zipper_lists(w, one)
+    # w -> 1 -> 2 -> 3
+
+def test_e():
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    one.next = two
+    two.next = three
+    # 1 -> 2 -> 3 
+
+    w = Node("w")
+    # w
+
+    return zipper_lists(one, w)
+    # 1 -> w -> 2 -> 3
+
 print(test_a())
 print(test_b())
 print(test_c())
-
+print(test_d())
+print(test_e())
