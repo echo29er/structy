24cff985901aaa557281d608ce85175322a5cb34
echo29er <sbassettuk@gmail.com>
2025-04-07 21:25:27 +0100

    new

---
+++ 05_graphs/20250331_connnected_components_count.py
@@ -0,0 +1,123 @@
+
+def connected_components_count(graph): 
+    """
+    Function Purpose: 
+        Write a function, connected_components_count, that takes in the adjacency list of an undirected graph. 
+        The function should return the number of connected components within the graph.
+    
+    Parameters:
+        graph (list): an adjaceny list of an undirected graph.
+
+    Returns:
+        int: The number of connected components in the graph.
+        
+    Assumptions: 
+        - There is at least one node in the graph.
+
+    Time complexity: 
+        O(e) number of edges
+
+    Space Complexity: 
+        O(n) number of nodes
+
+    """
+
+    """ 
+    INITIAL THOUGHTS 
+    * We need to build up a dictionary of the edges to create an adjacency list.
+    
+    
+    """
+    
+    # Handle empty graph
+    if not graph: 
+        return 0
+    
+    # Use a set to give O(1) insertion and look up
+    visited = set()
+    count = 0 # initialise the count
+
+    for node in graph: 
+        # node is the key of each dictionary
+        if explore(graph, node, visited) == True: 
+            count += 1
+    return count
+    
+def explore(graph, current, visited):
+    if current in visited: 
+        return False # for a component we've seen
+    
+    visited.add(current)
+
+    for neighbour in graph[current]:
+        explore(graph, neighbour, visited)
+
+    return True
+
+
+### TEST CASES
+
+def test_a():
+    return connected_components_count({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}) # -> 2
+
+
+def test_b():
+    return connected_components_count({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}) # -> 2
+
+
+def test_c():
+    return connected_components_count({
+  3: [],
+  4: [6],
+  6: [4, 5, 7, 8],
+  8: [6],
+  7: [6],
+  5: [6],
+  1: [2],
+  2: [1]
+}) # -> 3
+
+
+
+def test_d():
+   return connected_components_count({}) # -> 0
+
+
+
+def test_e():
+    return connected_components_count({
+  0: [4,7],
+  1: [],
+  2: [],
+  3: [6],
+  4: [0],
+  6: [3],
+  7: [0],
+  8: []
+}) # -> 5
+
+
+
+### EXECUTE TESTS
+
+print(test_a())
+print(test_b())
+print(test_c())
+print(test_d())
+print(test_e())
